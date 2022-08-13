914c0204728062ac78cdd3dab7ff189543227445
AoufiAbderahmane <aoufi.abderahmane@lastingdynamics.com>
2022-08-13 16:04:41 +0100

    start web server

---
+++ webserver/server.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const fs = require("fs");
+
+http
+    .createServer(function(request, response) {
+        fs.readFile("./index.html", "utf-8", (err, data) => {
+            if (err) {
+                return response.end("<h1>file not found please try agin later</h1>");
+            }
+            response.end(data);
+        });
+    })
+    .listen(3000, "192.168.1.51");
